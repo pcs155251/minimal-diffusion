9336c4d9a42b44fc5aa410824ff88dcfefc2f674
Julian Lee <pcs155251@gmail.com>
2024-09-23 15:59:47 +0800

    Add script to save sampled mnist data as image

---
+++ load_sampling_mnist.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+# path = '/home/julian/work/minimal-diffusion/trained_models/UNet_mnist-250-sampling_steps-500_images-class_condn_True.npz'
+# path = '/home/julian/work/minimal-diffusion/trained_models/0918/UNet_mnist-250-sampling_steps-500_images-class_condn_True.npz'
+path = '/home/julian/work/minimal-diffusion/sampled_images/UNet_mnist-250-sampling_steps-500_images-class_condn_True.npz'
+samples = np.load(path)
+
+for idx, image in enumerate(samples['arr_0.npy']):
+    grayImage = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    cv2.imwrite(f'sample_mnist_{idx}_{samples["arr_1.npy"][idx]}.png', grayImage)
+
