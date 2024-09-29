eee4f0091e91f6c901ec4b5b3cf8c811e3d984a1
Julian Lee <pcs155251@gmail.com>
2024-09-29 13:22:13 +0800

    Minor updates

--- print_digit.py
+++ print_digit.py
@@ -2,11 +2,12 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 import pathlib
 import itertools
+import numpy as np
 
 # Specify the directory where your fonts are located
 # font_dir = "path_to_your_fonts_directory"  # Change this to your font directory
 font_dir = '/usr/share/fonts/truetype/'
-output_dir = "./dataset/MNIST_printed10/"  # Output directory for images
+output_dir = "./dataset/MNIST_printed_simple_size12/"  # Output directory for images
 pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
 # List of digits to create images for
@@ -16,15 +17,28 @@ digits = [str(i) for i in range(10)]
 font_files = sorted(pathlib.Path(font_dir).rglob('*.ttf'))
 print(len(font_files))
 
+
 img_size = 28
 font_sizes = list(range(25, 29)) # 4
 angles = list(range(-15, 16, 5))
 shift_xs = list(range(4))
 shift_ys = list(range(4))
-print(f'number of combinations: {len(font_sizes) * len(angles) * len(shift_xs) * len(shift_ys)}')
+num_fonts = 10
+
+
+font_sizes = [12]
+angles = [0]
+shift_xs = np.arange(-5, 5.01, 0.1).tolist()
+shift_ys = np.arange(-5, 5.01, 0.1).tolist()
+num_fonts = 1
+
+num_combinations = len(font_sizes) * len(angles) * len(shift_xs) * len(shift_ys) * num_fonts
+print(f'number of combinations: {num_combinations}')
+print(f'num_files: {num_combinations*len(digits)}')
+
 
 # Generate images for each digit and each font
-for font_path in font_files[:10]:
+for font_path in font_files[:num_fonts]:
     for digit, font_size, angle, shift_x, shift_y in itertools.product(digits, font_sizes, angles, shift_xs, shift_ys):
         # Load the font
         font = ImageFont.truetype(font_path, font_size)
@@ -47,6 +61,6 @@ for font_path in font_files[:10]:
 
         # Save the image with a unique name based on digit and font name
         font_name = os.path.basename(font_path).split('.')[0]  # Get font name without extension
-        img.save(os.path.join(output_dir, f"{font_name}_{digit}_sz{font_size}_ang{angle}_x{shift_x}_y{shift_y}.png"))
+        img.save(os.path.join(output_dir, f"{font_name}_{digit}_sz{font_size}_ang{angle}_x{shift_x:.1f}_y{shift_y:.1f}.png"))
 
 print("Images created successfully!")
